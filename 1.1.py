bd197f238bc71cea77f71ebd27afca23d07441dc
bipnosik <ksenon237@gmail.com>
2024-09-14 15:04:10 +0300

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,31 @@
+import regex
+
+def is_palindrome(string):
+
+    if not isinstance(string, str):
+        return False
+
+
+    cleaned_string = regex.sub(r'[^a-zA-Z0-9]', '', str(string)).lower()
+
+
+    return cleaned_string == cleaned_string[::-1]
+print(is_palindrome("A man, a plan, a canal -- Panama"))
+print(is_palindrome("Madam, I'm Adam!"))
+print(is_palindrome(333))
+print(is_palindrome(None))
+print(is_palindrome("Abracadabra"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
