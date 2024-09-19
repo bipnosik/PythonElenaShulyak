f0bd82bfb715b736c7acc709a81e53342f0d40cd
bipnosik <ksenon237@gmail.com>
2024-09-19 22:30:20 +0300

    Update task_04.py

--- task_04.py
+++ task_04.py
@@ -4,17 +4,15 @@ def sort_list(list1):
 
     max_num = max(list1)
     min_num = min(list1)
-    max_numindex = list1.index(max_num)
-    min_numindex = list1.index(min_num)
+    result = [max_num if x == min_num else min_num if x == max_num else x for x in list1]
 
-    list1[max_numindex],list1[min_numindex]  = min_num,max_num
-    list1.append(min_num)
-    return list1
-
+    result.append(min_num)
+    return result
 print(sort_list([]))
 print(sort_list([2, 4, 6, 8]))
 print(sort_list([1]))
 print(sort_list([1, 2, 1, 3]))
+
 
 
 
