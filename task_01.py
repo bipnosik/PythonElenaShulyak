84e3b7a5f4755a9b5fff02a1b89dc7d121728cde
bipnosik <ksenon237@gmail.com>
2024-09-19 22:12:00 +0300

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,21 +1,17 @@
-import regex
-
 def is_palindrome(string):
     string = str(string)
     if not isinstance(string, str):
         return False
-
-
-    cleaned_string = regex.sub(r'[^a-zA-Z0-9]', '', str(string)).lower()
-
-
+        
+    cleaned_string = ''.join(char.lower() for char in string if char.isalnum())
     return cleaned_string == cleaned_string[::-1]
+
+
 print(is_palindrome("A man, a plan, a canal -- Panama"))
 print(is_palindrome("Madam, I'm Adam!"))
 print(is_palindrome(333))
 print(is_palindrome(None))
 print(is_palindrome("Abracadabra"))
-
 
 
 
