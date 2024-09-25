500ce25f9f06c496793ac581f451485316440cb1
bipnosik <ksenon237@gmail.com>
2024-09-25 20:21:20 +0300

    Update task_08.py

--- task_08.py
+++ task_08.py
@@ -1,4 +1,4 @@
-def multiply_numbers(inputs):
+def multiply_numbers(inputs= ""):
     if isinstance(inputs, str):
         digits = [int(d) for d in inputs if d.isdigit()]
         if digits:
@@ -11,14 +11,13 @@ def multiply_numbers(inputs):
         return multiply(inputs)
     else:
         return None
-
 def multiply(numbers):
     result = 1
     for num in numbers:
         result *= num
     return result
-
-print(multiply_numbers(None))
+    
+print(multiply_numbers())
 print(multiply_numbers('ss'))
 print(multiply_numbers('1234'))
 print(multiply_numbers('sssdd34'))
